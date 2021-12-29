9672f146ed79d483272a5074527fc6affcf7a3ff
HyungJooKim <hyungjookim12@gmail.com>
2021-12-29 22:06:29 +0900

    [:wrench: config] Test추가

---
+++ src/main/java/com/readydance/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.readydance.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing //Auditing 과 관련된 bean 이용가능
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
